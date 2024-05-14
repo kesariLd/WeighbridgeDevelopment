d000291a3f12e087c2263fbeb1c22bc4e37beabc
narendraLd <narendra.sahu@ldtech.in>
2024-05-14 14:50:12 +0530

    weighbridge initial report generation pushed

--- Weighbridge/src/main/java/com/weighbridge/weighbridgeoperator/controllers/WeighmentReportController.java
+++ Weighbridge/src/main/java/com/weighbridge/weighbridgeoperator/controllers/WeighmentReportController.java
@@ -6,10 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
@@ -19,9 +21,9 @@ public class WeighmentReportController {
 
     @Autowired
     private WeighmentReportService weighmentReportService;
-    @GetMapping("/report")
-    public ResponseEntity< Map<String, Map<String, List<WeighmentReportResponse>>>> generateWeighmentReport() {
-        Map<String, Map<String, List<WeighmentReportResponse>>> report = weighmentReportService.generateWeighmentReport();
+    @GetMapping("/report/{startDate}/{endDate}")
+    public ResponseEntity< Map<String, Map<String, List<WeighmentReportResponse>>>> generateWeighmentReport(@PathVariable LocalDate startDate,LocalDate endDate) {
+        Map<String, Map<String, List<WeighmentReportResponse>>> report = weighmentReportService.generateWeighmentReport(startDate,endDate);
         return ResponseEntity.ok(report);
     }
 }
--- Weighbridge/src/main/java/com/weighbridge/weighbridgeoperator/services/WeighmentReportService.java
+++ Weighbridge/src/main/java/com/weighbridge/weighbridgeoperator/services/WeighmentReportService.java
@@ -2,10 +2,11 @@ package com.weighbridge.weighbridgeoperator.services;
 
 import com.weighbridge.weighbridgeoperator.payloads.WeighmentReportResponse;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
 
 public interface WeighmentReportService {
 
-    public Map<String, Map<String, List<WeighmentReportResponse>>> generateWeighmentReport();
+    Map<String, Map<String, List<WeighmentReportResponse>>> generateWeighmentReport(LocalDate startDate, LocalDate endDate);
 }
