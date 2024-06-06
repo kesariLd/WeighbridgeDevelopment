4ee46d4af91c3e50e100956cf3c673aece19b0d5
SarojiniLd <sarojini.rath@ldtech.in>
2024-06-06 15:05:04 +0530

    Update ManagementDashboardService.java

--- Weighbridge/src/main/java/com/weighbridge/management/services/ManagementDashboardService.java
+++ Weighbridge/src/main/java/com/weighbridge/management/services/ManagementDashboardService.java
@@ -7,6 +7,7 @@ import com.weighbridge.management.payload.MaterialProductDataResponse;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
 
 public interface ManagementDashboardService {
     MaterialProductDataResponse getMaterialProductBarChartData(ManagementPayload managementRequest);
